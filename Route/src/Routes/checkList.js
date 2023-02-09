a0fdd601a0950bd47a2ef38c01a4cbef310ccff4
Marcelo1080p <juniooor.2015@gmail.com>
2023-02-08 21:59:46 -0300

    routes

---
+++ Route/src/Routes/checkList.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.get('checklists', ( req, res ) => {
+    console.log("Olá");
+    res.send('<h1>ROUTES</h1>');
+});
+
+module.exports = router;
